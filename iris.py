79d337977421fd00385e4c656bb4604021c1c3b3
samuca23 <samuelchiodini@hotmail.com>
2023-05-22 23:09:00 -0300

    iris

---
+++ iris.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.header("Iris Flower Prediction App")
+st.sidebar.header("UI Parameters")
+
+def ui_parameters():
+    sepal_length = st.sidebar.slider('Sepal Length', 4.3, 7.9, 5.0)
+    sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.2)
+    petal_length = st.sidebar.slider('Pedal length', 1.0, 6.9, 2.5)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.5)
+
+    data = {
+        'sepal_length' : sepal_length,
+        'sepal_width' : sepal_width,
+        'pedal_length' : petal_length,
+        'pedal_with' : petal_width
+    }
+
+    parametros = pd.DataFrame(data, index=[0])
+    return parametros
+
+df = ui_parameters()
+st.subheader("UI parâmetros")
+st.write(df)
+
+iris = datasets.load_iris()
+
+X = iris.data
+Y = iris.target
+
+rfc = RandomForestClassifier()
+rfc.fit(X, Y)
+
+previsao = rfc.predict(df)
+previsao_probabilidade = rfc.predict_proba(df)
+
+st.subheader("Rótulos de classificação")
+st.write(iris.target_names)
+
+st.subheader("Previsão")
+st.write(previsao)
+
+st.subheader("Previsão %")
+st.write(previsao_probabilidade)
